40e14ddba2cd8bc34a4bcf4364e0a457af14027e
adifens <dickyareda@gmail.com>
2021-01-25 22:20:32 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const double defaultMargin = 24.0;
+
+Color blackColor = Color(0xff161822);
+Color whiteColor = Color(0xffF7F8FB);
+Color greyColor = Color(0xff737583);
+Color blueColor = Color(0xff3B3DBF);
+Color backgroundColor = Color(0xffF7F8FB);
+
+TextStyle whiteTextStyle = GoogleFonts.tajawal(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle blackTextStyle = GoogleFonts.tajawal(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+);
+TextStyle greyTextStyle = GoogleFonts.tajawal(
+  color: greyColor,
+  fontWeight: FontWeight.w400,
+);
